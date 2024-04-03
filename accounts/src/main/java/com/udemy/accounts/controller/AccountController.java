239409355c2b47a21216585f896a4ce5bc64568c
thanhlam1211 <lambthe150099@fpt.edu.vn>
2024-04-03 19:07:57 +0700

    update: write Delete API for customer

--- accounts/src/main/java/com/udemy/accounts/controller/AccountController.java
+++ accounts/src/main/java/com/udemy/accounts/controller/AccountController.java
@@ -5,6 +5,7 @@ import com.udemy.accounts.dto.CustomerDto;
 import com.udemy.accounts.dto.ResponseDto;
 import com.udemy.accounts.service.IAccountsService;
 import lombok.AllArgsConstructor;
+import org.intellij.lang.annotations.Pattern;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -41,6 +42,20 @@ public class AccountController {
             return ResponseEntity
                     .status(HttpStatus.EXPECTATION_FAILED)
                     .body(new ResponseDto(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_UPDATE));
+        }
+    }
+    @DeleteMapping("/delete")
+    public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam
+                                                            String mobileNumber) {
+        boolean isDeleted = iAccountsService.deleteAccount(mobileNumber);
+        if(isDeleted) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_DELETE));
         }
     }
 }
--- accounts/src/main/java/com/udemy/accounts/service/impl/AccountServiceImpl.java
+++ accounts/src/main/java/com/udemy/accounts/service/impl/AccountServiceImpl.java
@@ -102,14 +102,19 @@ public class AccountServiceImpl implements IAccountsService {
         }
         return  isUpdated;
     }
-
     /**
      * @param mobileNumber - Input Mobile Number
      * @return boolean indicating if the delete of Account details is successful or not
      */
     @Override
     public boolean deleteAccount(String mobileNumber) {
-        return false;
+        Customer customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber)
+        );
+        accountRepository.deleteByCustomerId(customer.getCustomerId());
+        customerRepository.deleteById(customer.getCustomerId());
+        return true;
     }
+
 
 }
