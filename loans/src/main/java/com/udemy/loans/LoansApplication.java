a18ba3a1d994cd1a0e2cede1e0f2fc56e3233a30
thanhlam1211 <lambthe150099@fpt.edu.vn>
2024-04-06 14:52:59 +0700

    feat: adding loans services

---
+++ loans/src/main/java/com/udemy/loans/LoansApplication.java
@@ -0,0 +1,42 @@
+package com.udemy.loans;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.eazybytes.loans.controller") })
+@EnableJpaRepositories("com.eazybytes.loans.repository")
+@EntityScan("com.eazybytes.loans.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loans microservice REST API Documentation",
+				description = "Bank Loans microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Madan Reddy",
+						email = "lam@udemy.com",
+						url = "https://www.google.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.googke.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Bank Loans microservice REST API Documentation",
+				url = "https://www.google.com/swagger-ui.html"
+		)
+)
+public class LoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansApplication.class, args);
+	}
+}
