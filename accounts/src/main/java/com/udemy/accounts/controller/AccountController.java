4ad07fea7ac2f297c09ee5b38e97f809ee20ff5a
thanhlam1211 <lambthe150099@fpt.edu.vn>
2024-04-10 18:06:09 +0700

    update: using Enviroment to get enviroment variable in account service

--- accounts/src/main/java/com/udemy/accounts/controller/AccountController.java
+++ accounts/src/main/java/com/udemy/accounts/controller/AccountController.java
@@ -15,7 +15,9 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -30,18 +32,21 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(value = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 //@AllArgsConstructor
-//@RequiredArgsConstructor
+@RequiredArgsConstructor
 @Validated
 public class AccountController {
 
-    private IAccountsService iAccountsService;
+    private final IAccountsService iAccountsService;
 
-    public AccountController(IAccountsService iAccountsService) {
-        this.iAccountsService = iAccountsService;
-    }
+//    public AccountController(IAccountsService iAccountsService) {
+//        this.iAccountsService = iAccountsService;
+//    }
 
     @Value("${build.version}")
     private String buildVersion;
+
+    @Autowired
+    private Environment environment;
 
     //Brief description of create account API
     @Operation(
@@ -195,5 +200,30 @@ public class AccountController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(buildVersion);
+    }
+
+    @Operation(
+            summary = "Get Java version",
+            description = "Get Java versions details that is installed into accounts microservice"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/java-version")
+    public ResponseEntity<String> getJavaVersion() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(environment.getProperty("JAVA_HOME"));
     }
 }
